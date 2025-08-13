f4d9d41e7743f286dbcf930da256f2e396944a65
had13yys <hadleymarston1@gmail.com>
2025-08-13 23:25:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    // Ignore messages from bots
+    if (message.author.bot) return;
+
+    if (message.content === '!ping') {
+        message.reply('🏓 Pong!');
+    }
+});
+
+client.on('messageCreate', message => {
+    // Ignore messages from bots
+    if (message.author.bot) return;
+
+    if (message.content === '!chloe') {
+        message.reply('is a minge licker and a large slag (also an alcoholic)');
+    }
+});
+
+client.on('messageCreate', message => {
+    // Ignore messages from bots
+    if (message.author.bot) return;
+
+    if (message.content === '!diva') {
+        message.reply('## na na na diva is a diva ');
+
+    }
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
